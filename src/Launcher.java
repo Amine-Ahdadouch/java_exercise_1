80e1e78265f49f3700759eb491c4c7564884d60d
amine ahdadouch <amine.ahdadouch@epita.fr>
2022-02-10 10:41:13 +0100

    parti 3

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,8 +3,9 @@ public class Launcher{
     public static void main(String[] args){
         System.out.println("Bienvenue");
         Scanner scanner = new Scanner(System.in);
-        String msg = scanner.nextLine();
-        if (!msg.equals("quit")){
+        String msg = "";
+        while (!msg.equals("quit")){
+            msg = scanner.nextLine();
             System.out.println("Unknown command");
         }
     }
